2dcfe2fbd895596312fb3b54f7ff6b2764971fcd
lukhaiminh <lukhaiminh@gmail.com>
2025-10-30 03:53:25 +0000

    Deploying to gh-pages from @ lukhaiminh/portfolio@0ca9c174079a0383a3df269526fa49fa3a9efb95 🚀

---
+++ _nuxt/nkVeAqqi.js
@@ -0,0 +1,1 @@
+import{r as t}from"./DLRuk6SJ.js";import a from"./vRZhrzzh.js";import{x as e,G as n,d as s,w as c,E as i}from"./Be3NFH0Z.js";import"./iik6CYzq.js";const d={__name:"ProseCaution",setup(p){const o=e();return(r,f)=>(s(),n(a,{color:"error",icon:i(o).ui.icons.caution},{default:c(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
